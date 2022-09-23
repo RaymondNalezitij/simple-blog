7ce34a37a84e174cc64f452b700adb676ab3f2b0
Aldis Sarja <aldis.sarja@gmail.com>
2022-09-23 13:15:19 +0300

    Middleware for admin

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum', 'isAdmin'])->get('/admin', [App\Http\Controllers\AdminController::class, 'index']);
