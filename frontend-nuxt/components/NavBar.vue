c8b1d12b84681118ec1b3545ff9194714d26e7e8
Aldis Sarja <aldis.sarja@gmail.com>
2022-09-26 12:08:34 +0300

    Catch validation errors from backend

--- frontend-nuxt/components/NavBar.vue
+++ frontend-nuxt/components/NavBar.vue
@@ -1,12 +1,16 @@
 <template>
 <div>
-    <a slot="brand" class="navbar-brand" href="#">Navbar</a>
+    <a slot="brand" class="navbar-brand" href="/">Navbar</a>
     <nuxt-link :to="'/login'">
         Login
     </nuxt-link>
     <nuxt-link :to="'/admin/dashboard'">
         Admin Dashboard
     </nuxt-link>
+    <nuxt-link :to="'/'" v-if="this.$store.state.auth.auth">
+        {{ this.$store.state.auth.auth.name }}
+    </nuxt-link>
+
 
 </div>
   
--- frontend-nuxt/pages/Login.vue
+++ frontend-nuxt/pages/Login.vue
@@ -7,6 +7,11 @@
     </nuxt-link> -->
 
     <form @submit.prevent="onSubmit">
+      <div v-if="this.validationErrors">
+        <div class="error-messages" v-for="(message, error) in this.validationErrors">
+          {{ error }} : {{ message }}
+        </div>
+      </div>
       <div>
         <v-label>Email</v-label>
         <input type="text" v-model="user_email" placeholder="User Email" required>
@@ -30,26 +35,31 @@ export default {
   components: { Dashboard },
   data() {
     return {
-      //
+      validationErrors: null,
     }
   },
   methods: {
     async onSubmit() {
-        
-        const payload = {
-            email: this.user_email,
-            password: this.user_password,
-        };
 
-        try {
+      this.validationErrors = null;
+        
+      const payload = {
+        email: this.user_email,
+        password: this.user_password,
+      };
+
+      try {
         const response = await axios.post("http://127.0.0.1:8000/api/login", payload);
         this.$store.commit('auth/set', response.data.data);
         console.log("Loging OK!");
         console.log(this.$store.state.auth);
         
-        } catch (error) {
-            console.log(error);
-        }
+      } catch (error) {
+          console.log("LOGIN ERROR: ", error.response.data.errors);
+          if (error.response) {
+            this.validationErrors = error.response.data.errors;
+          }
+      }
     }
   }
 }
