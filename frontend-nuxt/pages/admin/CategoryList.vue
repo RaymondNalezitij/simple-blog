02e9ee3d93430fc145682c9dddebc717cc57bde7
Aldis Sarja <aldis.sarja@gmail.com>
2022-09-27 18:03:57 +0300

    the authorized user still can't request POST and PUT!

--- frontend-nuxt/pages/admin/CategoryList.vue
+++ frontend-nuxt/pages/admin/CategoryList.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <NavBar/>
-        {{ categories.length }}
+        
+        <form ref="formName" @submit.prevent="onSubmit">
+            <div class="add-category">
+                <input class="name-input" type="text" v-model="category_name" placeholder="Category" required>
+                <input class="button" type="submit" value="Add Category">
+            </div>
+        </form>
+
         <form v-for="category in categories" v-bind:key="category" :id="category.id">
 
             <input type="button" value="Remove"
@@ -28,14 +35,20 @@ export default {
 
     },
 
-    async created() {
+    created() {
+
+        this.fetchCategories();
+    },
+
+    methods: {
+
+        async fetchCategories() {
 
         const payload = {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Access-Control-Allow-Origin': '*',
-                // 'Authorization': 'Bearer ' + this.$store.state.auth.auth.token,
             }
         }
 
@@ -49,11 +62,8 @@ export default {
         }
     },
 
-    methods: {
-
         async removeCategory(event) {
-            // console.log("FROM REMOVE: ", event.explicitOriginalTarget.parentElement.id);
-            const id = event.explicitOriginalTarget.parentElement.id;
+            const id = event.target.parentElement.id;
                     const payload = {
             headers: {
                 'Content-Type': 'application/json',
@@ -68,16 +78,69 @@ export default {
                 + id,
                 payload
             );
+
+            this.fetchCategories();
+
         } catch (error) {
             console.log("TERRIBLE ERROR:", error);
         }
 
         },
 
-        changeCategory(event) {
-            const form = event.explicitOriginalTarget.parentElement;
-            console.log("FROM CHANGE:", form.category.value);
-        }
+        async changeCategory(event) {
+            const form = event.target.parentElement;
+            // console.log("FROM CHANGE:", form.category.value);
+            // console.log("FROM CHANGE:", event.target.parentElement);
+            const id = event.target.parentElement.id;
+            const name = form.category.value;
+            const payload = {
+                'name': name,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': 'Bearer ' + this.$store.state.auth.auth.token,
+                }
+            }
+
+            try {
+                await axios.put('http://127.0.0.1:8000/api/admin/categories/'
+                    + id,
+                    payload
+                    );
+
+                this.fetchCategories();
+
+            } catch (error) {
+                console.log("TERRIBLE ERROR:", error);
+            }
+
+        },
+
+        async onSubmit() {
+
+            const payload = {
+                'name': this.category_name,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': 'Bearer ' + this.$store.state.auth.auth.token,
+                }
+            }
+
+            try {
+            await axios.post('http://127.0.0.1:8000/api/admin/categories', payload);
+            } catch (error) {
+                console.log("TERRIBLE ERROR:", error);
+                return;
+            }
+
+            this.$refs.formName.reset();
+
+            this.fetchCategories();
+            
+        },
     }
 
 }
